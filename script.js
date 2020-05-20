c56f660c33020d2565b7a1ed0daf8c4814895461
Mads Terkildsen <mads.terkildsen2510@gmail.com>
2020-05-20 22:48:15 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+        this-currentOperand = ''
+        this-previousOperand = ''
+        this-operation = undefined
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationsButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearButtons = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+const Calculator =new Calculator(previousOperandTextElement, currentOperandTextElement)
